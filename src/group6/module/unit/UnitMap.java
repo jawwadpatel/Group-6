a20351b6d087c3c18342e234b7896c83a38d46bd
jawwadpatel <jawwadmemon89@gmail.com>
2016-09-08 11:31:05 +1000

    UnitMap.Java - Comments & Indentation
    I updated comments and indentation.

--- src/group6/module/unit/UnitMap.java
+++ src/group6/module/unit/UnitMap.java
@@ -1,5 +1,13 @@
-package group6.module.unit;
+    package group6.module.unit;
 
-import group6.IModule.IUnit;
+    import group6.IModule.IUnit;
+        /**
+         * @author      Abdul Jawwad Patel
+         * @version     1.0                 
+         * Module Class  
+         * It is used to handle course/subject details. Furthermore, it is used to evaluate 
+         * student's grade.
+         */
 
-public class UnitMap extends java.util.HashMap<String,IUnit> {}
+    // Create class and used hashmap for unique records
+    public class UnitMap extends java.util.HashMap<String,IUnit> {}
